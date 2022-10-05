3ef72c6e09249aa2546f8bcf46f21ab9b4a2e33d
Jan Piaskowy <piaskowyjasiek@gmail.com>
2022-10-05 08:46:38 +0200

    Midified exception handlers

--- Cinema Room REST Service/task/src/cinema/exceptions/exceptionsHandler/ControllerExceptionHandler.java
+++ Cinema Room REST Service/task/src/cinema/exceptions/exceptionsHandler/ControllerExceptionHandler.java
@@ -1,6 +1,8 @@
 package cinema.exceptions.exceptionsHandler;
 
 import cinema.domains.ErrorResponse;
+import cinema.exceptions.SeatDoesNotExistException;
+import cinema.exceptions.TicketAlreadyPurchasedException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -9,12 +11,30 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class ControllerExceptionHandler {
 
-    @ExceptionHandler(value = {Exception.class})
-    public ResponseEntity<ErrorResponse> globalExceptionHandler(Exception ex) {
-        ErrorResponse errorResponse = new ErrorResponse(
+    @ExceptionHandler(value = {SeatDoesNotExistException.class})
+    public ResponseEntity<ErrorResponse> seatDoesNotExistExceptionHandler(Exception ex) {
+        final ErrorResponse errorResponse = new ErrorResponse(
                 ex.getMessage()
         );
 
         return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
+
+    @ExceptionHandler(value = {TicketAlreadyPurchasedException.class})
+    public ResponseEntity<ErrorResponse> ticketAlreadyPurchasedException(Exception ex) {
+        final ErrorResponse errorResponse = new ErrorResponse(
+                ex.getMessage()
+        );
+
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(value = Exception.class)
+    public ResponseEntity<ErrorResponse> globalBaseExceptionHandler(Exception exception) {
+        final ErrorResponse errorResponse = new ErrorResponse(
+                exception.getMessage()
+        );
+        return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
 }
